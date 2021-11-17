7b315c7ce70756a8bf482206796c8aaf03ccb625
Bhanutejaror <93226957+Bhanutejaror@users.noreply.github.com>
2021-11-18 00:34:37 +0530

    Delete aws.rb

--- config/initializers/aws.rb
+++
@@ -1,6 +0,0 @@
-Aws.config.update({
-  region: 'ap-south-1',
-  credentials: Aws::Credentials.new(ENV['AKIAQGTXUQ3D2BV5QVY7'], ENV['mMKiZr4fByul2GFbQbU7m0RZgQAeyRMsOPENKoG0']),
-})
-
-S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['bhanu24teja'])
